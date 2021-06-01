23dda3d4cb0bc0f71812d854a9c15090f6d46127
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-01 20:30:11 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+a=5
+b=8
+#airthmatic operaters
+print("the value of a+b is",a+b)
+print("the value of a-b is",a-b)
+print("The value of a*b is",a*b)
+print("The value of a/b is",a/b)
+
+#assignment operators
+b=6
+b +=10
+print(b)
+b-=4
+print(b)
+b*=6
+print(b)
+b/=2
+print(b)
+
+#comparision operators
+c=(3>6)
+print(c)
+c=(3>=8)
+print(c)
+c=(6<=2)
+print(c)
+c=(6!=3)
+print(c)
+
+#logical operators
+Bool1=True
+Bool2=False
+print("The value of Bool1 and Bool2 is", (Bool1 and Bool2))
+
+a=(7>4)and(9<2)
+print(a)
