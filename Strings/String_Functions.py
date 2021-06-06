50e72475aa76ff648db9d802902795dd0c8b2070
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-06 20:36:43 +0530

    Create String_Functions.py

---
+++ Strings/String_Functions.py
@@ -0,0 +1,18 @@
+story="python is an interpreted high-level general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
+print(len(story))    #length of string
+
+#string ends with
+print(story.endswith("Happy"))
+
+#to find occurence
+print(story.count("p"))
+
+#to capitalize                              #will capitalize first character of the string
+print(story.capitalize())
+
+#to find a string
+print(story.find("Python"))
+
+#to replace a string
+print(story.replace("Python's","java"))              #first one: to replace, second: to be replaced by
+
