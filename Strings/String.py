f8ddc36e80535d5505728726bc6f6e4559053252
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-06 20:35:40 +0530

    Create String.py

---
+++ Strings/String.py
@@ -0,0 +1,29 @@
+#cancatenation
+Name="saksham"
+surname="Mudgal"
+print(Name + surname)
+
+#accessing individual element
+name="saksham"
+print(name[3])
+
+#slicing
+name="Saksham"
+print(name[1:4])
+
+#accessing using index sets
+var="Communication"
+print(var[0:4])
+print(var[2:7])
+
+print(var[:7])                 #starting from 0 index
+print(var[3:])                 #ending withj length-1
+
+#negative indexing
+var1="Authentication"
+print(var1[-6:-1])
+
+#slicing with skip value
+name="sakshammudgal"
+print(name[0::2])
+print(name[::3])
