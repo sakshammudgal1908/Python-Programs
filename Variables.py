43e522013df3b253d902d087502c011e742f088b
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-10 00:50:57 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,20 @@
+a='Tom and Jerry'
+b=1908
+c=2001.02
+d="Alastiare Kook"
+e='''Hey! What's goin wrong here'''
+f=True
+g=None
+g=False
+
+# printing values
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+#printing data types
+print(type(b))
