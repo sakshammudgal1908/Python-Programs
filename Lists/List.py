f2ee690b6a87be640293f2eb4167d8580cce3da7
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-09 20:32:24 +0530

    Create List.py

---
+++ Lists/List.py
@@ -0,0 +1,19 @@
+a=[3,6,8,14,19]
+print(a)
+print(a[0])
+print(a[1])
+
+#List can have heterogenous elements
+#name= ["Saksham",20,19//08//2002,"Bulandshahr"]
+name1= ["Saksham",20,"19 august 2002","Bulandshahr",120000.00,True]
+print(name1)
+print(name1[2])
+name1[3]='Ghaziabad'
+print(name1)
+
+#list slicing
+print(name1[:3])
+print(name1[-4:])
+
+#to calculate length
+print(len(name1))
