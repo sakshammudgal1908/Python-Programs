d05bccf5f9caac19dfd33159c51cfca75d7dea1d
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-12 12:24:04 +0530

    Create Tuples.py

---
+++ Tuples/Tuples.py
@@ -0,0 +1,26 @@
+
+#difference between tuple and list
+list1=[2,5,7,1,12]
+print(list1)
+
+list1[2]=54                     #updating value in list
+print(list1)
+
+tuple=(2,5,7,1,12)        #tuple having 5 elements
+print(tuple)
+
+tuple[2]=54
+print(tuple)                      #tuple do not support updation of elements
+
+#empty tuple
+tuple2=()
+print(tuple2)
+
+#tuple having single element
+tuple3=(10,)                         #correct syntax of declaring tuple having single element
+print(tuple3)
+
+#incoorect syntax
+#tuple3=(10)
+
+
