ca197e5ba2e6689b03ca157feaddbbe0632caec5
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-23 20:31:22 +0530

    Create For-Loop.py

---
+++ LoopS/For-Loop.py
@@ -0,0 +1,16 @@
+a=[12,34,48,64]
+for ages in a:
+    print(ages)
+
+#range function
+for a in range(21):
+    print(a)
+
+#specifying the starting value in range function
+for s in range(5,26):                # 5 is inclusive and 26 is exclusive
+    print(s)
+
+#taking 3rd argument in range function
+print("here we are printing some numbers")
+for s in range(1,20,3):
+    print(s)
