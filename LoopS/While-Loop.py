54d3ff5c99d67e14d0014db2aa18ff8fb86a19bb
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-06-24 00:32:52 +0530

    Create While-Loop.py

---
+++ LoopS/While-Loop.py
@@ -0,0 +1,26 @@
+#print 1 to 50 using while loop
+A=1
+while A<50:
+    print(A)
+    A=A+1
+print("program is successfully executed")
+
+#checking eligibility criteria
+i=18
+while i<=24:
+    print("his age is" ,i ," so,he can appear in exam")
+    i=i+1
+print("his age is greater than 24 so he can not appear in exam" 
+
+#printing my name
+a=0
+while a<5:
+    print("Rahul Dravid")
+    a=a+1
+
+#accessing the list using while loop
+cars=['audi','BMW','range rover','innova','fortuner']
+a=0
+while a<len(cars):
+    print(cars[a])
+    a=a+1
